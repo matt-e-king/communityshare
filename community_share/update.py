e0bc16da40710669305ee413a2f60df15e36f589
Ben Reynwar <ben@reynwar.net>
2014-05-01 15:31:27 -0700

    Added update script to add new tables.

---
+++ community_share/update.py
@@ -0,0 +1,9 @@
+from community_share.models.search import Label, Search
+from community_share.models.user import User
+from community_share.models.conversation import Conversation, Message
+from community_share.models.secret import Secret
+from community_share.store import Base, engine
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine);
+    
