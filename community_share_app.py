b1cee72c219aeac1269971f40fb472c9a5a9844f
Ben Reynwar <ben@reynwar.net>
2014-06-04 10:42:46 -0700

    Fixed bug in last commit.

--- community_share_app.py
+++ community_share_app.py
@@ -1,6 +1,6 @@
-from community_share import config, settings
+from community_share import config, settings, app
 config.load_from_environment()
 settings.setup_logging(config.LOGGING_LEVEL)
-app = make_app()
+app = app.make_app()
 app.debug = True
 app.run()
