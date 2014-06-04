735449c0a85da083ea121604ffe538b39b8f1490
Ben Reynwar <ben@reynwar.net>
2014-06-04 14:53:56 -0700

    Fixed bug in heroku script.

--- community_share_app.py
+++ community_share_app.py
@@ -1,12 +1,12 @@
 import logging
 
-from community_share import config, settings, app
+from community_share import config, app
 
 logger = logging.getLogger(__name__)
 
 logger.info('Loading settings from environment')
 config.load_from_environment()
 logger.info('Making application')
-app = make_app()
+app = app.make_app()
 app.debug = True
 logger.info('Debug={0}'.format(app.debug))
