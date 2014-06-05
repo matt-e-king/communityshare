a2dc9e16fb8520e1c7ee30b8977c9d0bba5202b0
Ben Reynwar <ben@reynwar.net>
2014-06-05 10:35:30 -0700

    Added datepicker for share date picking.

--- static/js/directives/mainview_directives.js
+++ static/js/directives/mainview_directives.js
@@ -9,7 +9,44 @@
     'csForbidden',
     function() {
       return {
-        templateUrl: '/static/templates/forbidden.html',
+        templateUrl: '/static/templates/forbidden.html'
+      };
+    });
+
+  module.directive(
+    'csDatepicker',
+    function() {
+      return {
+        scope: {
+          'ngModel': '='
+        },
+        templateUrl: '/static/templates/datepicker.html',
+        controller: function($scope) {
+
+          $scope.today = function() {
+            $scope.ngModel = new Date();
+          };
+          $scope.today();
+
+          $scope.clear = function () {
+            $scope.ngModel = null;
+          };
+
+          $scope.minDate = new Date();
+          $scope.maxDate = new Date();
+          $scope.maxDate.setFullYear($scope.maxDate.getFullYear()+1);
+          
+          $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.opened = true;
+          };
+
+          $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+          };
+        }
       };
     });
 
