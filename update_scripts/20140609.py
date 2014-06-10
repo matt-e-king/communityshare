9e9df4b6612d4e1e1db127a19bbaa8872b26a7f4
Ben Reynwar <ben@reynwar.net>
2014-06-09 22:14:05 -0700

    Adding more logging to update scripts.

--- update_scripts/20140609.py
+++ update_scripts/20140609.py
@@ -8,8 +8,11 @@ logger = logging.getLogger(__name__)
 def main():
     logger.info('Loading settings from environment')    
     config.load_from_environment()
+    logger.info('Creating new tables')
     setup.update_db()
+    logger.info('Getting the creating admin')
     creator = setup.get_creator()
+    logger.info('Updating the questions')
     setup.update_questions(get_questions(creator))
     store.session.commit()
 
