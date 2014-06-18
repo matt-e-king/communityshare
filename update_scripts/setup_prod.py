16324eddd7123888c16682230269d7b1155e21b1
Ben Reynwar <ben@reynwar.net>
2014-06-18 13:08:16 -0700

    Fixed bug in setup script.

--- update_scripts/setup_prod.py
+++ update_scripts/setup_prod.py
@@ -1,5 +1,11 @@
+import logging
+
+from community_share import config, setup
+
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     logger.info('Loading settings from environment')    
     config.load_from_environment()
     logger.info('Finished loading settings')
-    setup(n_random_users=0)
+    setup.setup(n_random_users=0)
