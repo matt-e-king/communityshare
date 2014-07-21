4c30056ebaa40f66b67bbe81c1ddc492648de315
Timmy Martin <timmywmartin@gmail.com>
2014-07-21 00:09:14 -0700

    Adding fields to Educator Signup Process

--- static/js/off-canvas.js
+++
@@ -1,9 +0,0 @@
-jQuery.(document).ready(function () {
-jQuery.('#push, #close').click(function () {
-var jQuery.navigacia = jQuery.('body, #slide-menu'),
-val = jQuery.navigacia.css('left') === '250px' ? '0px' : '250px';
-jQuery.navigacia.animate({
-left: val
-}, 300)
-});
-});
