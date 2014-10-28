689a574fda7b852e25664c1b9cbc8bdd7cfbeac6
asonnenschein <adrian.sonnenschein@gmail.com>
2014-10-27 22:08:05 -0700

    more label directives

--- static/js/directives/label_directives.js
+++ static/js/directives/label_directives.js
@@ -19,11 +19,16 @@
             communityPartnerSuggested: [
               'Science', 'Technology', 'Engineering', 'Math',
               'Visual Arts', 'Digital Media', 'Wildlife Biology', 'Literature',
-              'Performing Arts'
+              'Performing Arts', 'History', 'PE/Sports', 'Foreign Languages',
+              'Government', 'Health', 'Journalism', 'Culinary Arts',
+              'Entrepreneurship'
             ],
             educatorSuggested: [
-              'Social Studies', 'English/Language Arts', 'Foreign Languages', 'PE/Health/Sports',
-              'Mathematics', 'Goverment', 'Science'
+              'Science', 'Technology', 'Engineering', 'Math',
+              'Visual Arts', 'Digital Media', 'Wildlife Biology', 'Literature',
+              'Performing Arts', 'History', 'PE/Sports', 'Foreign Languages',
+              'Government', 'Health', 'Journalism', 'Culinary Arts',
+              'Entrepreneurship'
             ],
             other: []
           },
