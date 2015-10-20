1b40ce7db6e1f616e06ec4f684f3151f4239f56d
Ben Reynwar <ben@reynwar.net>
2015-10-19 20:56:47 -0700

    Fixed typo in restore db script.

--- puppet/modules/db/files/restore_db.sh
+++ puppet/modules/db/files/restore_db.sh
@@ -3,7 +3,7 @@
 if [ -f /vagrant/dump.sql ]; then
   if [ ! -f /home/vagrant/restored_dump ]; then
     su postgres;
-    pg_restore --verbose --clean --no-acl --no-owner -h localhost -d communityshare /vargrant/dump.sql;
+    pg_restore --verbose --clean --no-acl --no-owner -h localhost -d communityshare /vagrant/dump.sql;
     touch /home/vagrant/restored_dump;
     psql < /vagrant/update_scripts/modify201503.sql;
     python3 /vagrant/update_scripts/update_search.py;
