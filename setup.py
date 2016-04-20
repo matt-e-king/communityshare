c7e98f51ed1dea644610b1d482be2f9d7e259f5f
Sean Stephens <seanastephens@email.arizona.edu>
2016-04-19 19:14:01 -0700

    Change default number of users for setup.py

--- setup.py
+++ setup.py
@@ -300,5 +300,5 @@ if __name__ == '__main__':
     logger.info('Loading settings from environment')    
     config.load_from_file()
     logger.info('Finished loading settings')
-    setup(n_random_users=0)
+    setup(n_random_users=40)
               
